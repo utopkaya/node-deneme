e12e4b05ae931cd67666653593dc09730589f7e7
Umut Topkaya <tsg.umuttopkaya@gmail.com>
2021-03-06 13:04:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const hostname = 'localhost'
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req,res) => {
     res.send('merhaba kbu ieee!')
